48b4598ac61de988383263bfa339094947a33627
Jorge Alexandre <jorgeaamsantos@gmail.com>
2022-11-17 01:37:20 -0300

    add filtro basico

---
+++ assets/js/filtrar.js
@@ -0,0 +1,25 @@
+let inputFilter = document.querySelector("#filtro-tabela");
+
+inputFilter.addEventListener("input", function () {
+    console.log(this.value);
+    let clientes = document.querySelectorAll(".cliente");
+
+    if (this.value.length > 0) {
+        for (let i = 0; i < clientes.length; i++) {
+            let cliente = clientes[i];
+            let tdNome = cliente.querySelector(".info-nome");
+            let nome = tdNome.textContent;
+            if (nome != this.value) {
+                cliente.classList.add("invisivel")
+            } else {
+                cliente.classList.remove("invisivel")
+            }
+        }
+    } else {
+        for (let i = 0; i < clientes.length; i++) {
+            let cliente = clientes[i];
+            cliente.classList.remove("invisivel")
+        }
+
+    }
+})
